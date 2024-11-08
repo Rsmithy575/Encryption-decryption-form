34ac06e05a0501b385822b6ccd27bf5bfb8264fb
Rsmithy575 <Rsmithy575@gmail.com>
2024-11-08 16:21:49 +1100

    Add project files.

---
+++ battle cats cat log/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace PasswordEncryptionApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.textBoxKey = new System.Windows.Forms.TextBox();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecrypt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.BackColor = System.Drawing.Color.DarkOrange;
+            this.textBoxPassword.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.textBoxPassword.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(262, 182);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(312, 32);
+            this.textBoxPassword.TabIndex = 0;
+            this.textBoxPassword.Text = "Password";
+            // 
+            // textBoxKey
+            // 
+            this.textBoxKey.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.textBoxKey.Cursor = System.Windows.Forms.Cursors.NoMoveVert;
+            this.textBoxKey.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxKey.Location = new System.Drawing.Point(262, 269);
+            this.textBoxKey.Name = "textBoxKey";
+            this.textBoxKey.Size = new System.Drawing.Size(312, 32);
+            this.textBoxKey.TabIndex = 2;
+            this.textBoxKey.Text = "Key";
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.buttonSaveFile.Cursor = System.Windows.Forms.Cursors.PanNorth;
+            this.buttonSaveFile.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSaveFile.Location = new System.Drawing.Point(344, 344);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(139, 57);
+            this.buttonSaveFile.TabIndex = 9;
+            this.buttonSaveFile.Text = "Save";
+            this.buttonSaveFile.UseVisualStyleBackColor = false;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.buttonEncrypt.Cursor = System.Windows.Forms.Cursors.PanNorth;
+            this.buttonEncrypt.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonEncrypt.Location = new System.Drawing.Point(575, 344);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(139, 57);
+            this.buttonEncrypt.TabIndex = 8;
+            this.buttonEncrypt.Text = "Encrypt";
+            this.buttonEncrypt.UseVisualStyleBackColor = false;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecrypt
+            // 
+            this.buttonDecrypt.BackColor = System.Drawing.Color.MediumVioletRed;
+            this.buttonDecrypt.Cursor = System.Windows.Forms.Cursors.PanNorth;
+            this.buttonDecrypt.Font = new System.Drawing.Font("Gill Sans Ultra Bold", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDecrypt.Location = new System.Drawing.Point(121, 344);
+            this.buttonDecrypt.Name = "buttonDecrypt";
+            this.buttonDecrypt.Size = new System.Drawing.Size(139, 57);
+            this.buttonDecrypt.TabIndex = 7;
+            this.buttonDecrypt.Text = "Decrypt";
+            this.buttonDecrypt.UseVisualStyleBackColor = false;
+            this.buttonDecrypt.Click += new System.EventHandler(this.buttonDecrypt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.buttonDecrypt);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxKey);
+            this.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Encript/decryptor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.TextBox textBoxKey;
+        private System.Windows.Forms.Button buttonSaveFile;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecrypt;
+    }
+}
+
---
+++ battle cats cat log/Form1.cs
@@ -0,0 +1,263 @@
+﻿using System;
+
+using System.IO;
+
+using System.Security.Cryptography;
+
+using System.Text;
+
+using System.Windows.Forms;
+
+
+
+namespace PasswordEncryptionApp
+
+{
+
+    public partial class Form1 : Form
+
+    {
+
+        private string encryptedPassword = string.Empty;
+
+
+
+        public Form1()
+
+        {
+
+            InitializeComponent();
+
+        }
+
+
+
+        // When the Encrypt button is clicked 
+
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+
+        {
+
+            string password = textBoxPassword.Text;
+
+            string key = textBoxKey.Text;
+
+
+
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(key))
+
+            {
+
+                MessageBox.Show("Please enter both a password and a key.");
+
+                return;
+
+            }
+
+
+
+            encryptedPassword = EncryptPassword(password, key);
+
+            MessageBox.Show($"Encrypted Password: {encryptedPassword}");
+
+        }
+
+
+
+        // AES encryption 
+
+        private string EncryptPassword(string password, string key)
+
+        {
+
+            byte[] passwordBytes = Encoding.UTF8.GetBytes(password);
+
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key.PadRight(32).Substring(0, 32)); // Ensure 256-bit key 
+
+
+
+            using (Aes aes = Aes.Create())
+
+            {
+
+                aes.Key = keyBytes;
+
+                aes.Mode = CipherMode.CBC;
+
+                aes.Padding = PaddingMode.PKCS7;
+
+                aes.GenerateIV();
+
+
+
+                using (ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV))
+
+                {
+
+                    byte[] encryptedBytes = encryptor.TransformFinalBlock(passwordBytes, 0, passwordBytes.Length);
+
+                    byte[] combinedIvAndEncrypted = new byte[aes.IV.Length + encryptedBytes.Length];
+
+                    Array.Copy(aes.IV, 0, combinedIvAndEncrypted, 0, aes.IV.Length);
+
+                    Array.Copy(encryptedBytes, 0, combinedIvAndEncrypted, aes.IV.Length, encryptedBytes.Length);
+
+                    return Convert.ToBase64String(combinedIvAndEncrypted);
+
+                }
+
+            }
+
+        }
+
+
+
+        // When the Decrypt button is clicked 
+
+        private void buttonDecrypt_Click(object sender, EventArgs e)
+
+        {
+
+            string key = textBoxKey.Text;
+
+
+
+            if (string.IsNullOrEmpty(encryptedPassword) || string.IsNullOrEmpty(key))
+
+            {
+
+                MessageBox.Show("Please encrypt the password and provide a key first.");
+
+                return;
+
+            }
+
+
+
+            try
+
+            {
+
+                string decryptedPassword = DecryptPassword(encryptedPassword, key);
+
+                MessageBox.Show($"Decrypted Password: {decryptedPassword}");
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                MessageBox.Show("Decryption failed. Ensure the correct key is used.");
+
+            }
+
+        }
+
+
+
+        // AES decryption 
+
+        private string DecryptPassword(string encryptedPassword, string key)
+
+        {
+
+            byte[] combinedIvAndEncrypted = Convert.FromBase64String(encryptedPassword);
+
+            byte[] keyBytes = Encoding.UTF8.GetBytes(key.PadRight(32).Substring(0, 32));
+
+
+
+            using (Aes aes = Aes.Create())
+
+            {
+
+                aes.Key = keyBytes;
+
+                aes.Mode = CipherMode.CBC;
+
+                aes.Padding = PaddingMode.PKCS7;
+
+
+
+                byte[] iv = new byte[aes.BlockSize / 8];
+
+                byte[] encryptedBytes = new byte[combinedIvAndEncrypted.Length - iv.Length];
+
+
+
+                Array.Copy(combinedIvAndEncrypted, 0, iv, 0, iv.Length);
+
+                Array.Copy(combinedIvAndEncrypted, iv.Length, encryptedBytes, 0, encryptedBytes.Length);
+
+
+
+                aes.IV = iv;
+
+
+
+                using (ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV))
+
+                {
+
+                    byte[] decryptedBytes = decryptor.TransformFinalBlock(encryptedBytes, 0, encryptedBytes.Length);
+
+                    return Encoding.UTF8.GetString(decryptedBytes);
+
+                }
+
+            }
+
+        }
+
+
+
+        // Save the encrypted password to a text file 
+
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+
+        {
+
+            if (string.IsNullOrEmpty(encryptedPassword))
+
+            {
+
+                MessageBox.Show("Please encrypt the password first.");
+
+                return;
+
+            }
+
+
+
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.Filter = "Text File|*.txt";
+
+            saveFileDialog.Title = "Save Encrypted Password";
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+
+            {
+
+                using (StreamWriter writer = new StreamWriter(saveFileDialog.FileName))
+
+                {
+
+                    writer.WriteLine($"Encrypted Password: {encryptedPassword}");
+
+                }
+
+                MessageBox.Show("File saved successfully!");
+
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
